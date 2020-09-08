77c6d2a07ab18f6a0e136b06512bfb889317b0d2
JenVest2020 <jenvest2019@gmail.com>
2020-09-08 12:40:31 -0500

    looking through code for syntax mistakes

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./routers/server.js');
 
 
 server.listen(5000, () => {
-    console.log('server is listening on port 5000...');
+    console.log('\n***server is running on http://localhost:5000***\n');
 });
